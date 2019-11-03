cc3c8b588b4cce397db0ed20ebe352d4fd6da81a
Bryan_Torres_Calle <bryk347@gmail.com>
2019-11-02 22:35:13 -0500

    Avane_interface
    Primeros dos diseños de interfaces huecos prácticamente

--- Admin_Soft_Alpha/src/GUI/FXMLDocumentController.java
+++ Admin_Soft_Alpha/src/GUI/FXMLDocumentController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package admin_soft_alpha;
+package GUI;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -18,18 +18,9 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
-    @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }    
     
 }
