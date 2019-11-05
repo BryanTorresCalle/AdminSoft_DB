a8273613b61a224c97e5b97cf4795757a17e7e8d
Carlangas <carloszzzxc@hotmail.com>
2019-11-04 23:16:57 -0500

    Se agregan clases

--- Admin_Soft_Alpha/src/Classes/Company.java
+++ Admin_Soft_Alpha/src/Classes/Company.java
@@ -5,10 +5,59 @@
  */
 package Classes;
 
-/**
- *
- * @author Usuario
- */
 public class Company {
-    
+    int id;
+    String nit;
+    String nombre;
+    String correo;
+    String telefono;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Company(int id, String nit, String nombre, String correo, String telefono) {
+        this.id = id;
+        this.nit = nit;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.telefono = telefono;
+    }
+
 }
