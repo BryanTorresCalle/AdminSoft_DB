7824c08a18bcc7247af9d59a3af15a7648dc2ee4
Carlangas <carloszzzxc@hotmail.com>
2019-11-05 08:14:22 -0500

    Revert "Revert "Se crean las propiedades de los dueños""
    This reverts commit 0e017657b93892093aa9cfc7c14a1cddfe6e5b4e.

--- Admin_Soft_Alpha/src/Classes/Income.java
+++ Admin_Soft_Alpha/src/Classes/Income.java
@@ -10,5 +10,48 @@ package Classes;
  * @author Usuario
  */
 public class Income {
+    int id;
+    double value;
+    String paymentDate;
+    int apartmentId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public String getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(String paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public int getApartmentId() {
+        return apartmentId;
+    }
+
+    public void setApartmentId(int apartmentId) {
+        this.apartmentId = apartmentId;
+    }
+
+    public Income(int id, double value, String paymentDate, int apartmentId) {
+        this.id = id;
+        this.value = value;
+        this.paymentDate = paymentDate;
+        this.apartmentId = apartmentId;
+    }
     
 }
