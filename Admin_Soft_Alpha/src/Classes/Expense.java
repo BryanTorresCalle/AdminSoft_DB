65c7020b71ae164c89dde82ece65a759ea159f27
Carlangas <carloszzzxc@hotmail.com>
2019-11-04 23:24:28 -0500

    Se crean las propiedades de los dueños

--- Admin_Soft_Alpha/src/Classes/Expense.java
+++ Admin_Soft_Alpha/src/Classes/Expense.java
@@ -10,5 +10,58 @@ package Classes;
  * @author Usuario
  */
 public class Expense {
+    int id;
+    double value;
+    String paymentDate;
+    String idConcept;
+    String idCompany;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public String getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(String paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public String getIdConcept() {
+        return idConcept;
+    }
+
+    public void setIdConcept(String idConcept) {
+        this.idConcept = idConcept;
+    }
+
+    public String getIdCompany() {
+        return idCompany;
+    }
+
+    public void setIdCompany(String idCompany) {
+        this.idCompany = idCompany;
+    }
+
+    public Expense(int id, double value, String paymentDate, String idConcept, String idCompany) {
+        this.id = id;
+        this.value = value;
+        this.paymentDate = paymentDate;
+        this.idConcept = idConcept;
+        this.idCompany = idCompany;
+    }
     
 }
--- Admin_Soft_Alpha/src/Classes/Owner.java
+++ Admin_Soft_Alpha/src/Classes/Owner.java
@@ -10,5 +10,58 @@ package Classes;
  * @author Usuario
  */
 public class Owner {
+    int id;
+    int dni;
+    String name;
+    String eMail;
+    String phone; 
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
     
+
+    public Owner(int id, int dni, String name, String eMail, String phone) {
+        this.id = id;
+        this.dni = dni;
+        this.name = name;
+        this.eMail = eMail;
+        this.phone = phone;
+    }
 }
