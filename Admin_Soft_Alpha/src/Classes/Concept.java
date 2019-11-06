92d5a32a708566af76054f5849058a0b735bca9c
Carlangas <carloszzzxc@hotmail.com>
2019-11-06 08:47:29 -0500

    Create Concept.java

---
+++ Admin_Soft_Alpha/src/Classes/Concept.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Concept {
+    private final IntegerProperty id;
+    private final StringProperty description;
+    
+    public Concept(int id, String description){
+       this.id = new  SimpleIntegerProperty(id);
+       this.description = new SimpleStringProperty(description);
+    }
+    
+    public int getId(){
+        return id.get();
+    }
+    
+    public void setId(int id){
+        this.id.set(id);
+    }
+    
+    public IntegerProperty idProperty(){
+        return id;
+    }
+    
+    public String getDescription(){
+        return description.get();
+    }
+    
+    public void setDescription(String description){
+        this.description.set(description);
+    }
+    
+    public StringProperty descriptionProperty(){
+        return description;
+    }
+}
