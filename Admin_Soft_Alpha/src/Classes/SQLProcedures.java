c0eee91ad4d19249591f544af91f6fdc6e127c61
Carlangas <carloszzzxc@hotmail.com>
2019-11-06 08:58:39 -0500

    Create SQLProcedures.java

---
+++ Admin_Soft_Alpha/src/Classes/SQLProcedures.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Usuario
+ */
+public class SQLProcedures {
+    
+    private Connection getConnection() throws ClassNotFoundException, SQLException{
+        
+        Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/taller3def?"
+                    + "useTimeZone=true&serverTimeZone=UTC";
+            String user = "root";
+            
+            Connection myCon = DriverManager.getConnection(url,user,"");
+            return myCon;
+    }
+}
